a25c83fb1de53cc5661aae776617f597eea0add3
Dave Cohen <dcohen@infinio.com>
2015-07-21 14:22:11 -0400

    Bootstrap

---
+++ src/main/java/com/brave/resupply/controller/PageController.java
@@ -0,0 +1,32 @@
+package com.brave.resupply.controller;
+
+import com.brave.resupply.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dcohen on 2/13/15.
+ */
+@Controller
+public class PageController extends BaseController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("loggedIn", isLoggedIn());
+        return "index";
+    }
+
+    @RequestMapping("/new")
+    public String newCharacter(Model model) {
+        model.addAttribute("loggedIn", isLoggedIn());
+        if (isLoggedIn()) {
+            return "edit";
+        } else {
+            return "login";
+        }
+    }
+}
