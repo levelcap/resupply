c2fde7d1239d25bbbb368263024e6538d1985957
Dave Cohen <dcohen@infinio.com>
2015-07-21 15:50:49 -0400

    Login success to main page

--- src/main/java/com/brave/resupply/configuration/SecurityConfiguration.java
+++ src/main/java/com/brave/resupply/configuration/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 	.formLogin()
                 	.loginPage("/login")
-                	.defaultSuccessUrl("/user")
+                	.defaultSuccessUrl("/")
                 	.permitAll();
     }
 }
