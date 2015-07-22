71afa8dd99fe60c33547b509b6006b5131a02016
Dave Cohen <dcohen@moontoast.com>
2015-07-21 20:26:15 -0400

    Controller and logic update

--- src/main/java/com/brave/resupply/controller/OrderController.java
+++ src/main/java/com/brave/resupply/controller/OrderController.java
@@ -1,19 +1,23 @@
 package com.brave.resupply.controller;
 
-import com.brave.resupply.model.Order;
-import com.brave.resupply.model.User;
-import com.brave.resupply.repository.OrderRepository;
-import com.brave.resupply.repository.UserRepository;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.List;
+import com.brave.resupply.model.Order;
+import com.brave.resupply.model.User;
+import com.brave.resupply.repository.OrderRepository;
+import com.brave.resupply.repository.UserRepository;
 
 /**
  * Created by dcohen on 2/11/15.
